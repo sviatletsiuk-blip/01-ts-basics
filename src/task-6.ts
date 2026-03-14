ffc7defa067239cfeaa0cce6ba7792113aa93670
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-03-14 13:52:55 +0200

    2 commit

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,10 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-const number = getFirstElement([1, 2, 3]); // 1
-const string = getFirstElement(["a", "b", "c"]); // "a"
-const boolean = getFirstElement([true, false, true]); // true
+const num = getFirstElement<number>([1, 2, 3]); // 1
+const str = getFirstElement<string>(["a", "b", "c"]); // "a"
+const bool = getFirstElement<boolean>([true, false, true]); // true
+
+console.log(num);
+console.log(str);
+console.log(bool);
